2f1d72d7bee590a0dd83d9a5d6564610620efc7c
Thibault Morassin <thibault@kriptown.com>
2025-08-14 10:28:46 +0200

    build(front): integrate NativeWind and Tailwind CSS for styling

--- front/app/index.tsx
+++ front/app/index.tsx
@@ -1,15 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold">Welcome to Keepo!</Text>
     </View>
   );
 }
