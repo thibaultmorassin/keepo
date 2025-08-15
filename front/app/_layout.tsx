bdcf301f872f875b68ce87381c774808ff17ba0c
Thibault Morassin <thibault@kriptown.com>
2025-08-15 16:22:15 +0200

    fix(front): add react-native-safe-area-context to root layout

--- front/app/_layout.tsx
+++ front/app/_layout.tsx
@@ -1,6 +1,11 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack />
+    </SafeAreaProvider>
+  );
 }
