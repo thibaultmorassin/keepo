5668ccdbb47af22fdab3f8fc89a4de5508d877ba
Thibault Morassin <thibault@kriptown.com>
2025-08-18 09:19:45 +0200

    chore(front): configure Reanimated logger for improved debugging

--- front/app/_layout.tsx
+++ front/app/_layout.tsx
@@ -1,7 +1,16 @@
+import { AuthProvider } from "@/lib/cognito/auth.provider";
 import { Stack } from "expo-router";
+import {
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
+} from "react-native-reanimated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { AuthProvider } from "../lib/cognito/auth.provider";
 import "../global.css";
+
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false, // Reanimated runs in strict mode by default
+});
 
 export default function RootLayout() {
   return (
